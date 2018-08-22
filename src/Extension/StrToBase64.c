42c30cd01a6f9f7489bf7d0bbf8b770498a7e163
cshnik <cshnik@users.noreply.github.com>
2018-08-23 01:35:57 +0700

    #181 cleanup for 3rd party base64 files

--- src/Extension/StrToBase64.c
+++ src/Extension/StrToBase64.c
@@ -1,11 +1,182 @@
 #include <Windows.h>
+#include <assert.h>
 #include "Externals.h"
 #include "StrToBase64.h"
 #include "StringRecoding.h"
-#include "base64.h"
 
 extern StringSource ss = { 0 };
 extern RecodingAlgorythm ra = { 0 };
+
+static const unsigned char base64_table[65] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+BOOL CheckRequiredBase64DigitsAvailable(LPCSTR pCurTextOrigin, LPCSTR pCurText, const long iCurTextLength, const long nChars)
+{
+  BOOL res = FALSE;
+  if (iCurTextLength - (pCurText - pCurTextOrigin) >= nChars)
+  {
+    long i = 0;
+    res = TRUE;
+    for (i = 0; i < nChars; ++i)
+    {
+      res &= (strchr(base64_table, pCurText[i]) != 0);
+      if (!res)
+      {
+        break;
+      }
+    }
+  }
+  return res;
+}
+
+BOOL Base64_IsValidSequence(EncodingData* pED, const int requiredChars)
+{
+  return CheckRequiredBase64DigitsAvailable(pED->m_tb.m_ptr, pED->m_tb.m_ptr + pED->m_tb.m_iPos, pED->m_tb.m_iMaxPos, requiredChars);
+}
+
+static Base64Data b64data = { 0 };
+
+LPVOID Base64_InitAlgorythmData(const BOOL isEncoding)
+{
+  if (!isEncoding)
+  {
+    if (!b64data.initialized)
+    {
+      memset(b64data.dtable, 0x80, 256);
+      for (int i = 0; i < sizeof(base64_table) - 1; i++)
+      {
+        b64data.dtable[base64_table[i]] = (unsigned char)i;
+      }
+      b64data.dtable['='] = 0;
+      b64data.initialized = 1;
+    }
+    return (LPVOID)&b64data;
+  }
+  else
+  {
+    return NULL;
+  }
+}
+
+void Base64_ReleaseAlgorythmData(LPVOID pData)
+{
+}
+
+long Base64_GetEncodedCharLength(unsigned char ch)
+{
+  if (Is8BitEncodingMode())
+  {
+    return isascii(ch) ? 1 : 2;
+  }
+  return 1;
+}
+
+BOOL Base64_Encode(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed)
+{
+  unsigned char chInput[4] = { 0 };
+  for (int i = 0; i < _countof(chInput) - 1; ++i)
+  {
+    chInput[i] = TextBuffer_PopChar(&pED->m_tb);
+  }
+  TextBuffer_PushChar(&pED->m_tbRes, base64_table[chInput[0] >> 2]);
+  TextBuffer_PushChar(&pED->m_tbRes, base64_table[((chInput[0] & 0x03) << 4) | (chInput[1] >> 4)]);
+  TextBuffer_PushChar(&pED->m_tbRes, base64_table[((chInput[1] & 0x0f) << 2) | (chInput[2] >> 6)]);
+  TextBuffer_PushChar(&pED->m_tbRes, base64_table[chInput[2] & 0x3f]);
+
+  if (piCharsProcessed)
+  {
+    int iCharsProcessed = 0;
+    if (Is8BitEncodingMode())
+    {
+      for (int i = 0; i < _countof(chInput) - 1; ++i)
+      {
+        iCharsProcessed += Base64_GetEncodedCharLength(chInput[i]);
+      }
+    }
+    else
+    {
+      iCharsProcessed = _countof(chInput) - 1;
+    }
+    (*piCharsProcessed) += iCharsProcessed;
+  }
+  return TRUE;
+}
+
+BOOL Base64_EncodeTail(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed)
+{
+  long iCharsProcessed = 0;
+  unsigned char chInput[4] = { 0 };
+  int i = 0;
+
+  chInput[0] = TextBuffer_PopChar(&pED->m_tb);
+  iCharsProcessed += Base64_GetEncodedCharLength(chInput[0]);
+  TextBuffer_PushChar(&pED->m_tbRes, base64_table[chInput[0] >> 2]);
+
+  if (TextBuffer_GetTailLength(&pED->m_tb) == 0)
+  {
+    TextBuffer_PushChar(&pED->m_tbRes, base64_table[(chInput[0] & 0x03) << 4]);
+    TextBuffer_PushChar(&pED->m_tbRes, '=');
+  }
+  else
+  {
+    chInput[1] = TextBuffer_PopChar(&pED->m_tb);
+    iCharsProcessed += Base64_GetEncodedCharLength(chInput[1]);
+    TextBuffer_PushChar(&pED->m_tbRes, base64_table[((chInput[0] & 0x03) << 4) | (chInput[1] >> 4)]);
+    TextBuffer_PushChar(&pED->m_tbRes, base64_table[(chInput[1] & 0x0f) << 2]);
+  }
+  TextBuffer_PushChar(&pED->m_tbRes, '=');
+
+  if (piCharsProcessed)
+  {
+    (*piCharsProcessed) += iCharsProcessed;
+  }
+  return TRUE;
+}
+
+BOOL Base64_Decode(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed)
+{
+  Base64Data* pData = (Base64Data*)pRA->data;
+  assert(pData);
+
+  int iCharsProcessed = 0;
+  unsigned char chInput[5] = { 0 };
+  unsigned char block[5] = { 0 };
+  int pad = 0;
+  for (int i = 0; i < _countof(chInput) - 1; ++i)
+  {
+    if (!TextBuffer_GetLiteralChar(&pED->m_tb, &chInput[i], &iCharsProcessed))
+    {
+      return FALSE;
+    }
+    block[i] = pData->dtable[chInput[i]];
+    if (chInput[i] == '=')
+    {
+      pad++;
+    }
+  }
+  TextBuffer_PushChar(&pED->m_tbRes, (block[0] << 2) | (block[1] >> 4));
+  switch (pad)
+  {
+  case 0:
+    TextBuffer_PushChar(&pED->m_tbRes, (block[1] << 4) | (block[2] >> 2));
+    TextBuffer_PushChar(&pED->m_tbRes, (block[2] << 6) | block[3]);
+    break;
+  case 1:
+    TextBuffer_PushChar(&pED->m_tbRes, (block[1] << 4) | (block[2] >> 2));
+    break;
+  case 2:
+    break;
+  default:
+    assert(FALSE);
+    return FALSE;   // Invalid padding
+  }
+
+  if (piCharsProcessed)
+  {
+    (*piCharsProcessed) += iCharsProcessed;
+  }
+  return TRUE;
+}
 
 LPCSTR EncodeStringToBase64(LPCSTR text, const int textLength, const int encoding, const int bufferSize, int* pResultLength)
 {
--- src/Extension/StrToBase64.h
+++ src/Extension/StrToBase64.h
@@ -1,5 +1,23 @@
 #pragma once
-#include "base64.h"
+
+struct TBase64Data
+{
+  unsigned char initialized;
+  unsigned char dtable[256];
+};
+typedef struct TBase64Data Base64Data;
+
+struct TEncodingData;
+typedef struct TEncodingData EncodingData;
+struct TRecodingAlgorythm;
+typedef struct TRecodingAlgorythm RecodingAlgorythm;
+
+BOOL Base64_IsValidSequence(EncodingData* pED, const int requiredChars);
+LPVOID Base64_InitAlgorythmData(const BOOL isEncoding);
+void Base64_ReleaseAlgorythmData(LPVOID pData);
+BOOL Base64_Encode(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed);
+BOOL Base64_EncodeTail(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed);
+BOOL Base64_Decode(RecodingAlgorythm* pRA, EncodingData* pED, long* piCharsProcessed);
 
 #ifdef __cplusplus
 extern "C" { // C-Declarations
