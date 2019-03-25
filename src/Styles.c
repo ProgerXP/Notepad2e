302735ce15faa751655acd083f37f00a5738810f
cshnik <cshnik@users.noreply.github.com>
2019-03-26 01:08:46 +0700

    Awk syntax highlighting #216

--- src/Styles.c
+++ src/Styles.c
@@ -1626,11 +1626,39 @@ EDITLEXER lexMarkdown = { SCLEX_MARKDOWN, 63034, L"Markdown", L"markdown;mdown;m
 };
 
 
+KEYWORDLIST KeyWords_Awk = {
+    "break case continue default do else exit function for if in next return switch while "
+    "@include delete nextfile print printf BEGIN BEGINFILE END "
+    "atan2 cos exp int log rand sin sqrt srand asort asorti gensub gsub index "
+    "length match patsplit split sprintf strtonum sub substr tolower toupper close "
+    "fflush system mktime strftime systime and compl lshift rshift xor "
+    "isarray bindtextdomain dcgettext dcngettext",
+    "ARGC ARGIND ARGV FILENAME FNR FS NF NR OFMT OFS ORS RLENGTH RS RSTART SUBSEP TEXTDOMAIN "
+    "BINMODE CONVFMT FIELDWIDTHS FPAT IGNORECASE LINT TEXTDOMAiN ENVIRON ERRNO PROCINFO RT",
+    "", "", "", "", "", ""
+};
+
+
+EDITLEXER lexAWK = { SCLEX_PYTHON, 63035, L"Awk Script", L"awk", L"", &KeyWords_Awk,{
+        { STYLE_DEFAULT, 63126, L"Default", L"", L"" },
+        { SCE_P_WORD, 63128, L"Keyword", L"bold; fore:#0000A0", L"" },
+        { SCE_P_WORD, 63262, L"Keyword 2", L"bold; italic; fore:#6666FF", L"" },
+        { SCE_P_IDENTIFIER, 63129, L"Identifier", L"", L"" },
+        { MULTI_STYLE(SCE_P_COMMENTLINE,SCE_P_COMMENTBLOCK,0,0), 63127, L"Comment", L"fore:#808080", L"" },
+        { MULTI_STYLE(SCE_P_STRING,SCE_P_STRINGEOL,SCE_P_CHARACTER,0), 63131, L"String", L"fore:#008000", L"" },
+        { SCE_P_NUMBER, 63130, L"Number", L"fore:#C04000", L"" },
+        { SCE_P_OPERATOR, 63132, L"Operator", L"fore:#B000B0", L"" },
+        { -1, 00000, L"", L"", L"" }
+      }
+};
+
+
 PEDITLEXER pLexArray[NUMLEXERS] = {
     &lexDefault,
     &lexCONF,
     &lexASN1,
     &lexASM,
+    &lexAWK,
     &lexBASH,
     &lexBAT,
     &lexCS,
