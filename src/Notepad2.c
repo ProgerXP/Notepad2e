ece1d8906782e5645d496adbbec0360943052b3c
cshnik <cshnik@users.noreply.github.com>
2019-07-05 23:00:49 +0700

    #221: code cleanup (changed tabs to spaces)

--- src/Notepad2.c
+++ src/Notepad2.c
@@ -3338,8 +3338,8 @@ LRESULT MsgCommand(HWND hwnd, WPARAM wParam, LPARAM lParam)
       {
         char mszBuf[MAX_PATH] = { 0 };
         sprintf(mszBuf, "%05d", n2e_GenerateRandom());
-		SciCall_ReplaceSel(0, (LPARAM)mszBuf);
-		SciCall_SetSel(SciCall_GetAnchor() - strlen(mszBuf), SciCall_GetSelStart());
+        SciCall_ReplaceSel(0, (LPARAM)mszBuf);
+        SciCall_SetSel(SciCall_GetAnchor() - strlen(mszBuf), SciCall_GetSelStart());
       }
       break;
     // [/2e]
